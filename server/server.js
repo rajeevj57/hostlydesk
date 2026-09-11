f5e401a8565d73c27de7a2e5fe318e100b777467
rajeevj57 <81952348+rajeevj57@users.noreply.github.com>
2026-09-11 20:56:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,13 +5,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Middleware
 app.use(express.json());
-
-// Serve static frontend files from the root 'public' folder
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Database Setup (SQLite)
 const dbFile = path.join(__dirname, '../data/hostlydesk.db');
 const db = new sqlite3.Database(dbFile, (err) => {
   if (err) {
@@ -21,16 +17,14 @@ const db = new sqlite3.Database(dbFile, (err) => {
   }
 });
 
-// Routes pointing to the correct 'public' directory
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/kitchen', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/kitchen-dashboard.html'));
+  res.sendFile(path.join(__dirname, '../public/kitchen.html'));
 });
 
-// Start Server
 app.listen(PORT, () => {
   console.log(`HostlyDesk server running on port ${PORT}`);
 });
